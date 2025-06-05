de063f46152c0a5f5eb717d95b28e28f5e61cf23
Joao Pedro <73714674+Johny07159@users.noreply.github.com>
2025-06-05 10:58:26 -0300

    Add files via upload

---
+++ php/html/php/atividade33.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Resultado</h1>
+
+
+     <?php 
+
+    $numero_1 = $_GET['numero_1'];
+    
+    $numero_2 = 7;
+
+    $resultado_1 = $numero_1 * $numero_2 ;
+
+
+
+  echo "<p> O numero multiplicado por 7 sera : $resultado_1 </p><br>";
+ 
+
+    ?>
+
+
+</body>
+</html>
